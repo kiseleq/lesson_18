81edb596daef89fd7fbb7817073d72308615731a
Kiselev Andrey <kiseleq@gmail.com>
2021-03-20 12:44:39 +0300

    Урок 21

--- script/script.js
+++ script/script.js
@@ -196,6 +196,40 @@ window.addEventListener('DOMContentLoaded', function(){
 
     tabs();
 
+    // Точки
+
+    const showDots = () => {
+        const slide = document.querySelectorAll('.portfolio-item'),
+        dotsContainer = document.querySelector('.portfolio-dots');
+
+        let dots = 0;
+
+        const countDots = () => {
+            slide.forEach(() => {
+                dots++;
+            });
+        };
+        countDots();
+
+        const createDots = () => {
+            for (let i = 0; i < dots; i++) {
+                // let dot = document.createElement("li");
+                // dot.classList.add('active-dot');
+
+                // dot.append(dotsContainer);
+
+                // document.body.insertBefore(dotsContainer, dot);
+
+                dotsContainer.insertAdjacentHTML('afterbegin', '<li class = "dot"></li>');
+                let showDot = dotsContainer.querySelector('li');
+            }
+        };
+        createDots();
+        
+    };
+
+    showDots();
+
     // Слайдер
 
     const slider = () => {
